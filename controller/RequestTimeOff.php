e8c4d92299caac5fa05a524a2ecca69b877a9d2c
Jacob R <114666086+JustARaceThing@users.noreply.github.com>
2023-05-18 09:55:00 -0400

    more bug fixing

--- controller/RequestTimeOff.php
+++ controller/RequestTimeOff.php
@@ -7,15 +7,16 @@ error_reporting(E_ERROR);
 $db = new Database();
 
 //takes var from local index.php test file. needs replaced to adapt to wepage.
-$EmpName = $_REQUEST['name'];
-$EmpID = $_REQUEST['ID'];
-$StartDate = $_REQUEST['start'];
-$EndDate = $_REQUEST['end'];
-$Comments = $_REQUEST['comments'];
+$EmpName = $_POST['name'];
+$EmpID = (int)$_POST['EmpID'];
+$StartDate = $_POST['start'];
+$EndDate = $_POST['end'];
+$Comments = $_POST['comments'];
 
 
-//sets var to insert into table
+//sets var to insert into table.
 $insert = "INSERT INTO requests VALUES ('0', '$EmpName', '$EmpID', '$StartDate', '$EndDate', '$Comments')";
+
 
 //checks conn and executes query
 if (mysqli_query($db->getDbConn(), $insert)) {
