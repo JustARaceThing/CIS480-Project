e920d2f6c499c4906317bd477ad30ec230103f74
illsignflow <johnhendo98@gmail.com>
2023-05-19 14:00:34 -0400

    Index File
    Adjusted index.php file to reflect changes

--- index.php
+++ index.php
@@ -7,5 +7,7 @@
         <h1>Navigation Test</h1>
         <h2><a href="./view/conn_status.php">Test Connection</a></h2>
         <h2><a href="./view/login.php">Test Login</a></h2>
+        <h2><a href="./view/display_database.php">View Database</a></h2>
+        <h2><a href="./view/requestPage.php">View Request Page</a></h2>
     </body>
 </main>
