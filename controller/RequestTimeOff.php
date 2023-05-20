a87b5d5a807c55df7ff97ceefc08c80396dc7775
Jacob R <114666086+JustARaceThing@users.noreply.github.com>
2023-05-20 19:42:36 -0400

    Request page connected to front end

--- controller/RequestTimeOff.php
+++ controller/RequestTimeOff.php
@@ -19,7 +19,7 @@ $insert = "INSERT INTO requests VALUES ('0', '$EmpName', '$EmpID', '$StartDate',
 
 //checks conn and executes query
 if (mysqli_query($db->getDbConn(), $insert)) {
-  echo "Time off Request submitted!";
+  header("Location: ../view/requests.php");
 } else {
   echo "Error: " . $insert . "<br>" . mysqli_error($db->getDbConn());
 }
--- view/requestPage.php
+++
@@ -1,8 +0,0 @@
-<form method='POST' action='../controller/RequestTimeOff.php'> 
-            <h2>Name: <input type="text" name="name"></h2>
-            <h2>Employee ID: <input type="text" name="EmpID"></h2>
-            <h2>Start Date: <input type="date" name="start"></h2>
-            <h2>End Date: <input type="date" name="end"></h2>
-            <h2>Comments: <input type="text" name="comments"></h2>
-            <input type="submit" value="submit" name="submit">
-</form>
