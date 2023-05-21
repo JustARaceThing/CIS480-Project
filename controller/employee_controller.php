dbd424c99a752fe6e6a50f7dc038824b28766860
illsignflow <johnhendo98@gmail.com>
2023-05-21 17:22:51 -0400

    HRS 1.0
    Site should be working properly with these updates

--- controller/employee_controller.php
+++ controller/employee_controller.php
@@ -1,6 +1,6 @@
 <?php
 require_once('employee.php');
-require_once('../model/employee_db.php');
+require_once('/storage/ssd3/029/20738029/public_html/model/employee_db.php'); //absolute location on webhost
 
 class EmployeeController {
     //convert row from database into Employee object
--- view/schedule_page.php
+++ view/schedule_page.php
@@ -72,7 +72,6 @@
             border-style: solid;
             border-color: #fff;
         }
-         /*responsive*/
 
         @media (max-width: 600px){
             header{
@@ -93,8 +92,8 @@
         <img src="../images/logo.png" alt="logo" class="image1">
         <ul>
             <li><a href="home.php">Home</a></li>
-            <li><a href="schedule.php">Schedule</a></li>
-            <li><a href="requests.php">Requests</a></li>
+            <li><a href="schedule_page.php">Schedule</a></li>
+            <li><a href="request_page.php">Requests</a></li>
             <li><a href="resources.php">Resources</a></li>
             <li><a href="databases.php">Databases</a></li>
             <li><a href="directory.php">Employee Directory</a></li>
@@ -103,16 +102,9 @@
     </nav>
 
     <main>
-        <h2>Requests</h2>
+        <h2>Schedule</h2>
         <p>Welcome!</p>
-        <p><form method='POST' action='../controller/RequestTimeOff.php'> 
-            <h2>Name: <input type="text" name="name"></h2>
-            <h2>Employee ID: <input type="text" name="EmpID"></h2>
-            <h2>Start Date: <input type="date" name="start"></h2>
-            <h2>End Date: <input type="date" name="end"></h2>
-            <h2>Comments: <input type="text" name="comments"></h2>
-            <input type="submit" value="submit" name="submit">
-        </form></p>
+        <p>Here, you can view your past, current, and future schedule.</p>
     </main>
 
     <footer>
